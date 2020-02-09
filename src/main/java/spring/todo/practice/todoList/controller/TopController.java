8907bb2f28d1e3f125c10e8f2267c923ddc207a6
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-09 18:17:08 +0900

    add post process

--- src/main/java/spring/todo/practice/todoList/controller/TopController.java
+++ src/main/java/spring/todo/practice/todoList/controller/TopController.java
@@ -2,7 +2,10 @@ package spring.todo.practice.todoList.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TopController {
@@ -15,4 +18,11 @@ public class TopController {
         return "todo_list";
     }
 
+    @PostMapping("/todo")
+    public String postRequest(@RequestParam("registered_item")String str, Model model){
+
+        model.addAttribute("registeredItem", str);
+
+        return "todo_list";
+    }
 }
